3d5633c88b05310d32fd175b9077a5d716425242
Yuna1ee <ylee60@stuy.edu>
2024-04-17 14:16:53 -0400

    ww

--- fractals.py
+++ fractals.py
@@ -1,9 +1,10 @@
 import turtle
+import random
 window = turtle.Screen()
 
 def koch_curve(t, depth, size):
     if (depth == 1):
-        t.fd(size)
+        t.fd(random.randrange(size))
     else:
         koch_curve(t, depth-1, size)
         t.lt(60)
@@ -16,38 +17,7 @@ def koch_curve(t, depth, size):
 
 raphael = turtle.Turtle()
 raphael.pu()
-raphael.goto(-295, -290)
+raphael.goto(-450, 0)
 raphael.pd()
-koch_curve(raphael, 3, 100)
-
-def triangle(t, size):
-    t.lt(60)
-    t.fd(size)
-    t.rt(120)
-    t.fd(size)
-    t.rt(120)
-    t.fd(size)
-    t.rt(180)
-
-def sierpinski(t, depth, size, scale_factor=1):
-    if depth == 1:
-        triangle(t, size)
-    else:
-        sierpinski(t, depth-1, size/2)
-        t.fd(size/2)
-        sierpinski(t, depth-1, size/2)
-        t.bk(size/2)
-        t.lt(60)
-        t.fd(size/2)
-        t.rt(60)
-        sierpinski(t, depth-1, size/2)
-        t.rt(120)
-        t.fd(size/2)
-        t.lt(120)
-        
-raphael.pu()
-raphael.goto(-295, -290)
-raphael.pd()
-sierpinski(raphael, 5, 200)
-
+koch_curve(raphael, 4, 50)
 window.exitonclick()

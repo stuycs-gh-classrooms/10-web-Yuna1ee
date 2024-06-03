f68bd02f2cab3746f107d5070bac8ef3e37d46ee
Yuna1ee <ylee60@stuy.edu>
2024-06-03 15:32:26 -0400

    + quadratic

--- finalfrontier/calculator.py
+++ finalfrontier/calculator.py
@@ -1,14 +1,28 @@
 #! /usr/bin/python
 print('Content-type: text/html\n')
+#stats
+#trig
+#logs
+#sequences
+#solve rational functions
 
-def quadratic (string):
-    string = string.replace(' ', '')
-    splitters = ['+', '-']
-    for splitter in splitters:
-        string = string.split(splitter)
-        string = ' '.join(string)
-    string = string.split(' ')
+def quadratic (a, b, c): #returns roots of a quadratic
+    #abc from string
+    ans = ''
+    x0 = 0
+    x1 = 0
+    discriminant = ((b ** 2) - (4 * a * c)) ** (1/2)
+    left = (-1 * b) / (2 * a)
+    right = 0
+    if discriminant < 0:
+        return 'no roots'
+    else:
+        right = discriminant / (2 * a)
+        x0 = str(left + right)
+        x1 = str(left - right)
+    ans = 'The solutions are: ' + 'x = ' + x0 + ' and x = ' + x1
+    return ans
     
-    return string
-s = '2x2 + 3x + 1'
-print(quadratic(s))
+def         
+        
+    
